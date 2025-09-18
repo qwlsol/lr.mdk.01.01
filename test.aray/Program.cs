e727cdb89da2bddaa703a57fcf87398f3d88199b
qwlsol <milasoloveva273@gmail.com>
2025-09-18 10:21:53 +0300

    Add files via upload

--- test.aray/Program.cs
+++ test.aray/Program.cs
@@ -10,6 +10,43 @@ namespace test.aray
     {
         static void Main(string[] args)
         {
+            string[] aray = new string[3];
+            aray[0] = "A";
+            aray[1] = "B";
+            aray[2] = "C";
+
+            aray[1] = "не А";
+
+            Console.WriteLine(aray[0]);
+            Console.WriteLine(aray[1]);
+            Console.WriteLine(aray[2]);
+
+            string[] aray2 = new string[3];
+            aray2[0] = aray[2];
+            aray2[1] = aray[1];
+            aray2[2] = aray[0];
+
+            Console.WriteLine(aray2[0]);
+            Console.WriteLine(aray2[1]);
+            Console.WriteLine(aray2[2]);
+
+            int[] a = new int[10000];
+            for (int i = 0; i < 10000; i++)
+            { 
+                a[i] = i + 1;
+            }
+                Console.Write("[");
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                Console.Write($"{a[i]},");
+                
+            }
+            Console.Write("]");
+            {
+               
+            }
+               
         }
     }
 }
