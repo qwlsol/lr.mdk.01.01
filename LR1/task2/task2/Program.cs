5d2f74d99487d303989474cb00a43e81039a22db
adres <adres@DESKTOP-B33T6BV>
2025-09-25 16:13:13 +0300

    создание объекта генератора случайных чисел и объявление массива с размером 3000 элементов

---
+++ LR1/task2/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///вариант 5
+
+            Random rnd = new Random();
+            int[] random = new int[3000];
+
+        }
+    }
+}
