64b707ca8c7c226249d4933e01602db6e6b3c7ab
Vladimir420911 <dhbtgvhb@gmail.com>
2025-09-27 12:59:45 +0300

    /

---
+++ function/function/ListStringsModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function
+{
+    class ListStringsModule
+    {
+        static public List<string> StringsModule()
+        {
+            List<string> list = new List<string>();
+
+            Console.Write("Введите количество строк: ");
+            while (true)
+            {
+                string i = Console.ReadLine();
+                list.Add(i);
+                break;
+            }
+            return list;
+        }
+        static public List<int> Search(List<string> StringsModule, string text)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < StringsModule.Count; i++)
+            {
+
+            }
+            
+            return result;
+        }
+
+        
+    }
+}
---
+++ function/function/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = ListStringsModule.StringsModule();
+            Console.WriteLine("Последний элемент списка:" + name[name.Count-1]);
+            Console.WriteLine("Размер списка: " + name.Count);
+
+
+        }
+    }
+}
