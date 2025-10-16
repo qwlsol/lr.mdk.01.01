7ff9f4ad4a770aef1399b1bfc60058d4c59e3b39
Vladimir420911 <dhbtgvhb@gmail.com>
2025-10-16 11:11:43 +0300

    Создание перечисления WeekDay

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum WeekDay
+    {
+        Sunday,   
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    class Program
+    {
+        static void Main()
+        { 
+           
+
+        }
+    }
+}
