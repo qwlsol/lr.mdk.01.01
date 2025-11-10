0b51ea47b1976d07a05aa172a13406f1c740ddf1
guzer <guzer@DESKTOP-K1P33T2>
2025-11-10 21:05:57 +0300

    возвращаем список моделей телефонов и список продаж

--- LR2/LR2/LR2/SearchingModule.cs
+++ LR2/LR2/LR2/SearchingModule.cs
@@ -30,6 +30,8 @@ namespace LR2
 
             phonesByBrand = allPhones[indexBrand];
             salesByBrand = counts[indexBrand];
+
+            return (phonesByBrand, salesByBrand);
         }
     }
 }
