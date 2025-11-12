a31b541a53bee2a6a0bbbde61beef36a7397173c
guzer <guzer@DESKTOP-K1P33T2>
2025-11-12 21:26:01 +0300

    создаем функцию для вычисления суммы общего за период X

---
+++ MobilePhoneSales/MobilePhoneSales/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhoneSales
+{
+    internal class Program
+    {
+        static int AllSums(int[] price, int[] AmountSaled, DataTime[] Time)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
