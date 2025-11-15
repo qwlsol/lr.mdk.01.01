d1b2d911693e38ca7b9555a5c961d1e16a39aad6
guzer <guzer@DESKTOP-K1P33T2>
2025-11-15 14:21:57 +0300

    через цикл for проверяем есть ли оценки, если есть складываем все оценки в переменную sum, далее делим сумму на количество оценок и возвращаем результат

--- L2new/Applicant.cs
+++ L2new/Applicant.cs
@@ -16,8 +16,13 @@ namespace L2new
         {
             if (grades == null || grades.Length == 0)
                 return 0;
-
             double sum = 0;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                sum += grades[i];
+            }
+            return sum / grades.Length;
+
         }
     }
 }
