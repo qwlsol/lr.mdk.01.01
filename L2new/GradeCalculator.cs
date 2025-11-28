0a8251fbf6ce6df491e6430b00c372e02546e898
guzer <guzer@DESKTOP-K1P33T2>
2025-11-28 21:44:05 +0300

    создаем функцию для вычисления среднего балла(получаем массив оценок, складываем все оценки, делим на количество оценок, возвращаем среднее значение)

--- L2new/GradeCalculator.cs
+++ L2new/GradeCalculator.cs
@@ -8,5 +8,14 @@ namespace L2new
 {
     internal class GradeCalculator
     {
+        public double CalculateAverage(int[] grades)
+        {
+            double sum = 0;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                sum += grades[i];
+            }
+            return sum / grades.Length;
+        }
     }
 }
