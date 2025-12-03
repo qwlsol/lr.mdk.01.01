654fdf934aa752572eeb03687247e2cf137050d0
Anon1m4k <kirilvalera2@gmail.com>
2025-12-03 08:42:53 +0300

    создаем методы для введения и выведения информации

--- TasksClasses/Task1.1/Employee.cs
+++ TasksClasses/Task1.1/Employee.cs
@@ -8,10 +8,34 @@ namespace Task1._1
 {
     public abstract class Employee
     {
-        private int id;
-        private string fullName;
-        private string email;
+        private int id_;
+        private string fullName_;
+        private string email_;
 
+        public void SetId(int id)
+        {
+            id_ = id;
+        }
+        public int GetId()
+        {
+            return id_;
+        }
+        public void SetFullName(string fullName) 
+        {
+            fullName_ = fullName;
+        }
+        public string GetFullName() 
+        {
+            return fullName_;
+        }
+        public void SetEmail(string email)
+        {
+            email_ = email;
+        }
+        public string GetEmail()
+        {
+            return email_;
+        }
 
     }
 }
