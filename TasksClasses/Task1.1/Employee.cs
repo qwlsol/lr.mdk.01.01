76d482997804793833881b8ef9361fa5734e7a16
Anon1m4k <kirilvalera2@gmail.com>
2025-12-03 08:38:12 +0300

    Создаем абстрактный(может быть использован только как базовый класс при наследовании) класс для сотрудников. В нем обьявляем поля  `Id`, `FullName`, `Email`

---
+++ TasksClasses/Task1.1/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._1
+{
+    public abstract class Employee
+    {
+        private int id;
+        private string fullName;
+        private string email;
+
+
+    }
+}
