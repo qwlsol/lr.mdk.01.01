06974cf5da38360f3b9268aa4e8b9f9de773b232
Anon1m4k <kirilvalera2@gmail.com>
2025-12-03 09:20:34 +0300

    Создаем класс почасовая заработная плата сотрудника, добавляем внего часовую ставку, часы и сверхурочные часы. В абстрактный метод рассчета зарплаты добавляем вычисления: почасовая оплата + сверхурочные

---
+++ TasksClasses/Task1.1/HourlySalaryEmployee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._1
+{
+    class HourlySalaryEmployee : Employee
+    {
+        private double hourRate_;
+        private double hours_;
+        private double overtimeHours_;
+
+        public void SetHourRate(double hourRate)
+        {
+            hourRate_ = hourRate;
+        }
+        public double GetHourRate() 
+        {
+            return hourRate_;        
+        }
+        public void SetHours(double hours) 
+        {
+            hours_ = hours;
+        }
+        public double GetHours()
+        {
+            return hours_;
+        }
+        public void SetOvertimeHours(double overtimeHours)
+        {
+            overtimeHours_ = overtimeHours;
+        }
+        public double GetOvertimeHours()
+        { 
+            return overtimeHours_; 
+        }
+        public override double CalculateSalary()
+        {
+            return (hours_ * hourRate_) + (overtimeHours_ * hourRate_ * 1.5 );
+        }
+    }
+}
