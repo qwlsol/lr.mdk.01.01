b362e6a7424be0af83a0843e2e3ce60b1c6d4a46
Anon1m4k <kirilvalera2@gmail.com>
2025-12-03 11:04:33 +0300

    Создаем класс для круга. Добавляем ему радиус. так же добавляем  метод `Accept(IVisitor visitor)

---
+++ TasksClasses/Task1.2/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._2
+{
+    class Circle
+    {
+        private double radius_;
+
+        public Circle(double radius)
+        {
+            if (radius <= 0) 
+            radius_ = radius;
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit();
+        }
+    }
+}
+
