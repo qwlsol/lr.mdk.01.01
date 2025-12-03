c185d21eae97480e62b75d22e44d3d92d9bd85e7
Anon1m4k <kirilvalera2@gmail.com>
2025-12-03 09:05:26 +0300

    Создаем производный класс (наследуется от класса Сотдуники) "фиксированная месячная оплата", в кготором создаем месяную зарплату с методами добавления и выведения. и добавляем бстрактный метод рассчета зарплаты из класса сотрудники( унаследованный с помощь ключевого слова override) и возращаем в этом методе месячную зарплату

---
+++ TasksClasses/Task1.1/FixedSalaryEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._1
+{
+    public class FixedSalaryEmployee : Employee
+    {
+        private double monthySalary;
+
+        public void SetMonthySalary(double salary)
+        { monthySalary = salary;}
+        public double GetMonthySalary()
+        { return monthySalary; }
+
+        public override double CalculateSalary()
+        {
+            return monthySalary;
+        }
+    }
+}
