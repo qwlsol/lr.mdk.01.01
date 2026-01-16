2109b67b6d520b53334db09988c21d8dc01e8950
Z <pgbc303@gmail.com>
2026-01-16 10:08:59 +0300

    проект в формах с таблицей

---
+++ PropertiesApp/PropertiesWinForms/PropertiesWinForms/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PropertiesWinForms
+{
+    public partial class MainForm: Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Что-то нажали");
+        }
+
+        private void ShowButton_Click(object sender, System.EventArgs e)
+        {
+            /// 0. Определить источник данных (предполагаем, что это будет список студентов)
+            /// 1. Определить столбцы таблицы (атрибуты объектов)
+            /// В данном контексте это будут: ФИО, дата рождения, номер билета, средний балл
+            /// 2. Перенести данные источника в виде строк в таблицу,
+            /// причем важно соблюдать порядок следования данных в соотвествие с порядком столбцов.
+
+
+            /// Задача #0
+            List<Student> allStudents = new List<Student>();
+
+            Student st1 = new Student();
+            st1.Name = "Иванов Иван Иванович";
+            st1.SetBirthDay(new System.DateTime(1997, 11, 27));
+            st1.Number = "ТК/13-ку";
+            st1.SetAvgBall(4.5);
+
+            Student st2 = new Student();
+            st2.Name = "Петрова Ольга Дмитриевна";
+            st2.SetBirthDay(new System.DateTime(2000, 12, 31));
+            st2.Number = "ТК/17-ку";
+            st2.SetAvgBall(3.3);
+
+            allStudents.Add(st1);
+            allStudents.Add(st2);
+
+            StudentsTable.DataSource = allStudents;
+        }
+    }
+}
