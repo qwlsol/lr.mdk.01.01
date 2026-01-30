eecf27f5c53d25e71e9f513a64be6e1c56523e7d
guzer <guzer@DESKTOP-K1P33T2>
2026-01-30 21:04:52 +0300

    Создаем класс для блюда, добавляем в него переменные, список ингредиентов и фото

---
+++ LR3/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3
+{
+    internal class Dish
+    {
+        public string name;
+        public string description;
+        public decimal price;
+        public List<string> ingredients;
+        public string imagePath;
+    }
+}
