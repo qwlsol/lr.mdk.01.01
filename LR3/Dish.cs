23a1b8da208392e3ff2eb9241ed4cc10d7fb4c39
Z <pgbc303@gmail.com>
2026-01-30 10:30:34 +0300

    создаем конрструктор для класса блюд

--- LR3/Dish.cs
+++ LR3/Dish.cs
@@ -12,5 +12,14 @@ namespace LR3
         private string description_;
         private decimal price_;
         private string ingredients_;
+
+        public Dish(string name, string description, decimal price, string ingredients)
+        {
+            name_ = name;
+            description_ = description;
+            price_ = price;
+            ingredients_ = ingredients;
+        }
+
     }
 }
