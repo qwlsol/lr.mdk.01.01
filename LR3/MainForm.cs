70c8d0096963ae11c69e38d6a909a71b1d80b899
Z <pgbc303@gmail.com>
2026-01-30 10:36:31 +0300

    создаём словари, первый будет хранить категории меню, а второй количество заказов

--- LR3/MainForm.cs
+++ LR3/MainForm.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace LR3 
 {
     public partial class MainForm : Form
     {
+        private Dictionary<string, List<Dish>> menuCategories;
+        private Dictionary<string, int> orderQuantities;
         public MainForm()
         {
             InitializeComponent();
