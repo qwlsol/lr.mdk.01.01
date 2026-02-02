07e75854b06522ee79d6244fcc8ae4627f622cf8
guzer <guzer@DESKTOP-K1P33T2>
2026-02-02 21:43:05 +0300

    добавляем перемнные с характеристиками блюд и создаем для них конструктор класса

--- LR3/Dish.cs
+++ LR3/Dish.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace LR3
 {
-    internal class Dish
+    public class Dish
     {
+        private string name_;
+        private double price_;
+        private string description_;
+        private string ingredients_;
+
+        public Dish(string name, double price, string description, string ingredients)
+        {
+            name_ = name;
+            price_ = price;
+            description_ = description;
+            ingredients_ = ingredients;
+        }
     }
 }
