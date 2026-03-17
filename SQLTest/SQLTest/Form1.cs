320779705dc8f6a0ec33cc53f1298af4ae3504cd
KateSmirnova1 <20katerina0060@gmail.com>
2026-03-17 11:19:22 +0300

    обработка sql базы данных в проект Windows Forms

---
+++ SQLTest/SQLTest/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace SQLTest
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            /*var cs = "Host=192.168.1.48;Username=st50-12;Password=5012;Database=P-30_Student";
+                 
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+            var sql = "SELECT login,password FROM users";
+            var cmd = new NpgsqlCommand(sql, con);
+            var reader = cmd.ExecuteReader();
+            List<string> login = new List<string>();
+            while (reader.Read())
+            {
+
+                 string log = reader.GetString(0);
+                
+                string pass = reader.GetString(1);
+                login.Add(log + ":"+ pass);
+
+
+            }
+            MessageBox.Show($"Логин : Пароль: { string.Join(", ", login)}");*/
+        }
+    }
+}
---
+++ SQLTest/SQLTest/pgUsersLoader.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLTest
+{
+    public class PgUsersLoader
+    {
+        List<User> allusers = new List<User>();
+        public List<User> Load()
+        {
+            var cs = "Host=192.168.1.48;Username=st50-12;Password=5012;Database=P-30_Student";
+
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+            var sql = "SELECT login,password,age,name FROM users";
+            var cmd = new NpgsqlCommand(sql, con);
+            var reader = cmd.ExecuteReader();
+            List<string> login = new List<string>();
+            while (reader.Read())
+            {
+
+                string log = reader.GetString(0);
+
+                string pass = reader.GetString(1);
+                login.Add(log + ":" + pass);
+
+
+            }
+            MessageBox.Show($"Логин : Пароль: {string.Join(", ", login)}");
+            return allusers;
+            
+        }
+        
+    }
+}
